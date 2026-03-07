65f690793458ce014680af36b05329537e4bbc3c
Shadow01 <68017917+ShadowVapes@users.noreply.github.com>
2026-03-08 00:01:09 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,6 @@
-export const SUPABASE_URL = 'sb_publishable_S_aZ48M0aNxt7QLrprHeyw_2ELvz-83';
-export const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpYWpsYXJ0cXdmeWd5ZWp2em5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2Mzk4NTgsImV4cCI6MjA4ODIxNTg1OH0.mHDpTvmTLcaZ-VhgBDoRhZYeOMZamJWnEzYaevhX_YE';
+export const SUPABASE_URL = 'https://ciajlartqwfygyejvznl.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpYWpsYXJ0cXdmeWd5ZWp2em5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2Mzk4NTgsImV4cCI6MjA4ODIxNTg1OH0.mHDpTvmTLcaZ-VhgBDoRhZYeOMZamJWnEzYaevhX_YE';
 
-export const DEMO_MODE = SUPABASE_URL.includes('YOUR_PROJECT') || SUPABASE_ANON_KEY.includes('YOUR_SUPABASE');
+export const DEMO_MODE =
+  SUPABASE_URL.includes('YOUR_PROJECT') ||
+  SUPABASE_ANON_KEY.includes('YOUR_SUPABASE');
