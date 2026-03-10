312a64205b9a8bd21b96a2d5ae92fd7a862c6ceb
Shadow01 <68017917+ShadowVapes@users.noreply.github.com>
2026-03-10 22:14:56 +0100

    Delete assets directory

--- assets/config.js
+++
@@ -1,6 +0,0 @@
-export const SUPABASE_URL = 'https://ciajlartqwfygyejvznl.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjaWFqbGFydHF3ZnlneWVqdnpubCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzcyNjM5ODU4LCJleHAiOjIwODgyMTU4NTh9.mHDpTvmTLcaZ-VhgBDoRhZYeOMZamJWnEzYaevhX_YE';
-
-export const DEMO_MODE =
-  SUPABASE_URL.includes('YOUR_PROJECT') ||
-  SUPABASE_ANON_KEY.includes('YOUR_SUPABASE');
